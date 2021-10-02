f8e618b7746dc811742b65fa84594423d46058f7
sergiobollo <58012294+sergiobollo@users.noreply.github.com>
2021-10-02 04:55:05 -0300

    se creo el componente poll-vote

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Poll } from './types';
 
 @Component({
   selector: 'app-root',
@@ -7,18 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   showForm = false;
+  activePoll: Poll = null;
 
-  polls = [{
+  polls: Poll[] = [{
+    id: 1,
     question: 'Do you like dogs or cats?',
-    image: 'https://images.pexels.com/photos/1909802/pexels-photo-1909802.jpeg',
-    votes: [0, 5, 7],
+    thumbnail: 'https://images.pexels.com/photos/1909802/pexels-photo-1909802.jpeg',
+    results: [0, 5, 7],
+    options: ['Cats', 'Dogs', 'None'],
     voted: true
   },
   {
+    id: 2,
     question: 'Best month for summer holidays?',
-    image: 'https://images.pexels.com/photos/38238/maldives-ile-beach-sun-38238.jpeg',
-    votes: [1, 6, 4],
+    thumbnail: 'https://images.pexels.com/photos/38238/maldives-ile-beach-sun-38238.jpeg',
+    results: [1, 6, 4],
+    options: ['June', 'July', 'August'],
     voted: false
   }
   ];
+
+  setActivePoll(poll) {
+    this.activePoll = null;
+    setTimeout(() => {
+      this.activePoll = poll;
+    }, 100);
+  }
 }
